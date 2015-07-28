192c739a7450e45117e852bf11a5726da204e752
brad <lyman.brad3211@gmail.com>
2015-07-28 17:35:07 -0400

    created a more sophisticated tokenizer

--- src/dnainterpreter/tokenizer.js
+++ src/dnainterpreter/tokenizer.js
@@ -1,5 +1,70 @@
-var tokenizer = function(code_string) {
-  return code_string.trim().split(/\s+/);
+var Immutable = require('immutable');
+
+/**
+ * Takes raw source code as a string and returns
+ * the code split up as lines with corresponding line
+ * numbers.
+ * Returns;
+ *   List of objects of the form { lineNum: n, tokens: [] }
+ **/
+var _splitOnLines = function(sourceAsString) {
+  var rawSplit = sourceAsString.split("\n");
+
+  return rawSplit
+    .map(function(line, i) {
+      return {
+        lineNum : i+1,
+        tokens  : line.match(/\S+/g)
+      };
+    })
+    .filter(function(rowObj) {
+      return rowObj.tokens !== null;
+    });
 };
 
-module.exports = tokenizer;
+/**
+ * Takes the list of line objects output by _splitOnLines
+ * and transforms it into a list of token objects.
+ * Returns:
+ *   List of objects of the form { lineNum: n, token: [] }.
+ **/
+var _linesToTokenStack = function(lineTokens) {
+  var tokens = [];
+
+  lineTokens.forEach(function(lineToken) {
+    lineToken.tokens.forEach(function(token) {
+      tokens.unshift({
+        lineNum : lineToken.lineNum,
+        value   : token
+      });
+    });
+  });
+
+  return Immutable.fromJS(tokens).toStack();
+};
+
+/**
+ * Internal module functions, exported for testing.
+ **/
+module.exports._private = {
+  splitOnLines      : _splitOnLines,
+  linesToTokenStack : _linesToTokenStack
+};
+
+/**
+ * The tokenize function which takes the source code and returns
+ * a Stack of tokens where the top of the stack is the bottom of
+ * the file.
+ **/
+module.exports.tokenize = function(source) {
+  return _linesToTokenStack(_splitOnLines(source));
+};
+
+
+
+
+
+
+
+
+
--- test/dnainterpreter/tokenizerTest.js
+++ test/dnainterpreter/tokenizerTest.js
@@ -1,16 +1,21 @@
-var exports   = module.exports,
+var Immutable = require('immutable'),
     tokenizer = require('../../src/dnainterpreter/tokenizer.js');
 
-var testStrings = {
-  splitOnWhitespace: "a \t b \n c \r\n d     e  \t"
-};
+var validSource = "cond\n1 2 >\n3 2 <\n2 *.up =\n \n \nstart\n1 4 add\n3 mult\nadd\n.10 store\nstop";
 
-exports.testSplitOnSpaces = function(test) {
-  test.expect(1);
+module.exports.testSplitOnLines = function(test) {
+  var tokenizedLines = tokenizer._private.splitOnLines(validSource);
 
-  var splitString = tokenizer(testStrings.splitOnWhitespace);
-  test.equal(splitString.length, 5, "String not broken on whitespace");
-
+  test.equals(tokenizedLines.length, 10, "validSource should split into 10 lines");
+  test.equals(tokenizedLines[4].tokens[0], "start", "line 5 should be the start keyword");
   test.done();
 };
 
+module.exports.testLinesToTokenStack = function(test) {
+  var tokenizedLines = tokenizer._private.splitOnLines(validSource),
+      tokenStack     = tokenizer._private.linesToTokenStack(tokenizedLines);
+
+  test.ok(Immutable.Stack.isStack(tokenStack));
+  test.equals(tokenStack.peek().get("value"), "stop", "the first token should be the last token in the source");
+  test.done();
+};
