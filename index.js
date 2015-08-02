360fae63a890dceb7b4465a49dbca7921e0b012f
brad <lyman.brad3211@gmail.com>
2015-08-02 19:32:48 -0400

    added index file

---
+++ index.js
@@ -0,0 +1,7 @@
+var parser = require('./src/dnainterpreter/parser.js'),
+    tokenizer = require('./src/dnainterpreter/tokenizer.js');
+
+module.exports = {
+  Parser    : parser,
+  Tokenizer : tokenizer
+};
