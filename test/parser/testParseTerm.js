b7e70fd805367e9f1e5f34dffd56ac7ea0d83e51
brad <lyman.brad3211@gmail.com>
2015-09-23 11:52:58 -0400

    term parser and tests completed

--- test/parser/testParseTerm.js
+++ test/parser/testParseTerm.js
@@ -9,5 +9,56 @@ module.exports.positiveTerms = {
     test.ok(!result.get_ok().isNegative, "Expected literal to be positive");
 
     test.done();
+  },
+
+  parseVariable : function(test) {
+    var positiveVariableString = "Eye1";
+    var result = termParser.parseTerm(positiveVariableString);
+
+    test.ok(
+      !result.is_err(),
+      "Expected variable to parse as " + positiveVariableString
+    );
+    test.ok(!result.get_ok().isNegative, "Expected variable to be positive");
+    test.done();
   }
 };
+
+module.exports.negativeTerms = {
+  parseLiteral : function(test) {
+    var negativeLiteralString = "-32.24";
+    var result = termParser.parseTerm(negativeLiteralString);
+
+    test.ok(!result.is_err(), "Expected negative literal to parse");
+
+    var termNode = result.get_ok();
+
+    test.ok(termNode.isNegative, "Expected literal to be negative");
+    test.equal(
+      termNode.value.value, 32.24,
+      "Expected Value node to equal 32.24"
+    );
+    test.done();
+  },
+
+  parseVariable : function(test) {
+    var negativeVariableString = "-Eye2";
+    var result = termParser.parseTerm(negativeVariableString);
+
+    test.ok(!result.is_err(), "Expected negative variable to parse");
+
+    var termNode = result.get_ok();
+
+    test.ok(termNode.isNegative, "expected variable to be negative");
+    test.equal(
+      termNode.value.value, "Eye2",
+      "Expected Variable name to be correct"
+    );
+    test.done();
+  }
+};
+
+
+
+
+
