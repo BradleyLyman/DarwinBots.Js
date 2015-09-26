ff971d031b802a62ba6f241a3b23c6f5b588e730
brad <lyman.brad3211@gmail.com>
2015-09-26 08:16:13 -0400

    demo now shows some basic timing information on the executed source

--- demo.js
+++ demo.js
@@ -18,7 +18,17 @@ fs.readFile('./demo.dbs', 'utf8', function(err, source) {
       console.log(sysvars);
       compiledSource(sysvars);
 
-      console.log(sysvars);
+      var timesToRun = 1000;
+      console.log("Executing source " + timesToRun + " times.");
+      var start = process.hrtime();
+      for (var i = 0; i < timesToRun; i++) {
+        compiledSource(sysvars);
+      }
+      var duration = process.hrtime(start);
+
+      var totalTime = duration[0] * 1000 + duration[1]*1e-6;
+      console.log("duration: " + totalTime + " milliseconds");
+      console.log("time per dna execution: " + totalTime / timesToRun + " milliseconds");
     })
     .or_else(function(err) {
       console.log(err);
