9b78f7efd0a504491cac076983db3bba82fd3f1b
brad <lyman.brad3211@gmail.com>
2015-09-27 09:11:54 -0400

    started working on the simulation code

---
+++ src/simulation/Species.js
@@ -0,0 +1,29 @@
+/**
+ * Methods and objects for managing a DarwinBots.js simulation.
+ * @module Simulation
+ **/
+
+
+/**
+ * @class Species
+ * @property {String} rawSource - The species' raw dna source code.
+ * @property {String} name - The species' name.
+ * @property {Bool} isValid - True if the dna compiles successfully.
+ * @property {Species~DnaCallback} dnaCmd - Function for executing the species'
+ *                                          dna.
+ **/
+
+/**
+ * Creates a new Species object.
+ * @param {String} rawSource - The species' raw dna source code.
+ * @param {String} name - The species' name.
+ * @return {@type Simulation~Species}
+ **/
+module.exports.createSpecies = function(rawSource, name) {
+
+};
+
+
+
+
+
