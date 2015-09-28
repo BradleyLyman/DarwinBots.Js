029fb182b2ba77a3a13d4405691cfddae869ab47
brad <lyman.brad3211@gmail.com>
2015-09-28 11:55:38 -0400

    created Bot typedef

---
+++ src/simulation/Bot.js
@@ -0,0 +1,14 @@
+/**
+ * @module Simulation
+ **/
+
+/**
+ * @typedef Bot
+ * @type {Object}
+ * @property {Species} species - The bot's species.
+ * @property {Object} sysvars - The bot's sysvars.
+ * @property {Number} nrg - The bot's current energy level, when this reaches
+ *                          zero the bot dies.
+ **/
+
+
