ad5310e859d17423ae86be0cc26cf8d2c496696e
brad <lyman.brad3211@gmail.com>
2015-09-30 07:12:39 -0400

    DarwinBots exports Simulation directly, documentation updated in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-var parser = require('./parser/index.js'),
-    Simulation = require('./simulation/Simulation.js'),
+/**
+ * Contains the methods and objects for creating and
+ * running a DarwinBots.js simulation.
+ * @module DarwinBots
+ **/
+
+var Simulation = require('./simulation/Simulation.js'),
     Species    = require('./simulation/Species.js');
 
-module.exports.compileSource = parser.compileSource;
-module.exports.Simulation    = Simulation;
-module.exports.Species       = Species;
+/**
+ * Simulation module.
+ **/
+module.exports = Simulation;
+
+/**
+ * Simulation/Species module.
+ **/
+module.exports.Species = Species;
 
