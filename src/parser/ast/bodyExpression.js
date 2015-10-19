ae7a98f76a0d74c70deea540d927a66a604bb2da
brad <lyman.brad3211@gmail.com>
2015-10-19 12:08:35 -0400

    moved the body expressiont into its own file

---
+++ src/parser/ast/bodyExpression.js
@@ -0,0 +1,52 @@
+/**
+ * Exports a factory function for creating a body expression from a variable
+ * and an expression.
+ * @module Parser/Ast/bodyExpression
+ **/
+let typenames = require('./typenames.js');
+
+/** @class BodyExpression **/
+let BodyExpression = {};
+
+BodyExpression.prototype = {
+  /** @lends BodyExpression **/
+
+  /**
+   * The type of this node -- useful for debugging and
+   * pretty-printing.
+   * @return {String} This node's type name.
+   **/
+  type : function() { return typenames.BODY_EXPRESSION; },
+
+  /**
+   * Executes this node in the dna.
+   * @param {Object} sysvars - Object representing the bot's state to the dna.
+   **/
+  execute : function(sysvars) {
+    sysvars[this.variable.name] = this.expression.execute(sysvars);
+  },
+
+  /**
+   * Parses this node into a human-readable string.
+   * @return {String} Pretty-printed string.
+   **/
+  toString : function() {
+    return "BodyExpression: " + this.variable.toString() + " <- " +
+      this.expression.toString();
+  },
+};
+
+/**
+ * Creates a node representing a body expression.
+ * @param {Variable} variable - A node representing a variable.
+ * @param {Expression} expression - An expression who's value will be stored
+ *                                  in the variable.
+ * @return {BodyExpression} The body expression variable <- expression.
+ **/
+module.exports.createBodyExpression = function(variable, expression) {
+  return {
+    __proto__  : BodyExpression.prototype,
+    variable   : variable,
+    expression : expression,
+  };
+};
--- src/parser/ast/typenames.js
+++ src/parser/ast/typenames.js
@@ -5,10 +5,17 @@
 let keyMirror = require('keymirror');
 
 let astTypes = keyMirror({
-  DNA        : null,
-  GENE       : null,
-  EMPTY_COND : null,
-  AND_PHRASE : null
+  DNA          : null,
+  GENE         : null,
+  EMPTY_COND   : null,
+  AND_PHRASE   : null,
+  OR_PHASE     : null,
+  E_COND_EXPR  : null,
+  G_COND_EXPR  : null,
+  L_COND_EXPR  : null,
+  GE_COND_EXPR : null,
+  LE_COND_EXPR : null,
+  NE_COND_EXPR : null,
 });
 
 module.exports = astTypes;
