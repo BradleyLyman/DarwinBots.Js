bb8fb2bf23f25d286f0083cac35b40e1485e8a51
brad <lyman.brad3211@gmail.com>
2015-10-20 07:59:16 -0400

    updated ast.js to export all of the ast node factory functinos

--- src/parser/ast.js
+++ src/parser/ast.js
@@ -1,7 +1,40 @@
-let createBodyExpression = require('./bodyExpression.js'),
-    condExpressions      = require('./condExpressions.js'),
-    createDna            = require('./dna.js'),
-    createEmptyCond      = require('./emptyCond.js'),
-    createGene           = require('./gene.js'),
-    phrases              = require('./phrases.js');
+/**
+ * Exports methods for constructing all AST node types.
+ * @module Parser/Ast
+ **/
 
+let bodyExpression  = require('./ast/bodyExpression.js'),
+    condExpressions = require('./ast/condExpressions.js'),
+    dna             = require('./ast/dna.js'),
+    emptyCond       = require('./ast/emptyCond.js'),
+    gene            = require('./ast/gene.js'),
+    phrases         = require('./ast/phrases.js'),
+    expressions     = require('./ast/expressions.js');
+
+module.exports = {
+  createBodyExpression : bodyExpression.createBodyExpression,
+
+  createECondExpr  : condExpressions.createECondExpr,
+  createLCondExpr  : condExpressions.createLCondExpr,
+  createGCondExpr  : condExpressions.createGCondExpr,
+  createGECondExpr : condExpressions.createGECondExpr,
+  createLECondExpr : condExpressions.createLECondExpr,
+  createNECondExpr : condExpressions.createNECondExpr,
+  createEmptyCond  : emptyCond.createEmptyCond,
+
+  createDna : dna.createDna,
+
+  createGene : gene.createGene,
+
+  createAndPhrase : phrases.createAndPhrase,
+  createOrPhrase  : phrases.createOrPhrase,
+
+  createLiteral    : expressions.createLiteral,
+  createVariable   : expressions.createVariable,
+  createMulExpr    : expressions.createMulExpr,
+  createDivExpr    : expressions.createDivExpr,
+  createAddExpr    : expressions.createAddExpr,
+  createSubExpr    : expressions.createSubExpr,
+  createPowExpr    : expressions.createPowExpr,
+  createUMinusExpr : expressions.createUMinusExpr,
+};
